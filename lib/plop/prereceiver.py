af58d501e9a421d2e5897256f1ce0b1697566eca
Tom Howe <turtlebender@gmail.com>
2012-04-20 09:12:15 -0500

    Generate version using gitversion.
    Associate a build with a version number which is generated
    using the gitversion module.  This necessitates a change in
    the message format.

--- lib/plop/prereceiver.py
+++ lib/plop/prereceiver.py
@@ -7,12 +7,13 @@ import tempfile
 from clint import piped_in
 import contextlib
 import zmq
+from plop import gitversion
 
 CREATE_VIRTUALENV_CMD = 'virtualenv --distribute {0}'
 GIT_ARCHIVE_CMD = 'git archive {0} | tar -x -C {1}'
 PIP_INSTALL_CMD = '{0}/bin/pip install -i {1} --no-deps -r requirements.txt'
 RELOCATABLE_VENV_CMD = 'virtualenv --relocatable {0}'
-TAR_CMD = 'tar -czvf /tmp/{0}.tar.gz -C {1} {2}'
+TAR_CMD = 'tar -czvf /tmp/{0}-{1}.tar.gz -C {2} {3}'
 DEF_MIRROR = 'http://pypi.utils.globuscs.info/simple'
 DEF_SOCKET = 'ipc:///tmp/build_complete'
 
@@ -45,12 +46,14 @@ def build(project_name, newrev, socket_addr, pip_mirror):
             subprocess.check_call(pip_install, shell=True)
         relocatable_venv = RELOCATABLE_VENV_CMD.format(virtualenv_dir)
         subprocess.check_call(relocatable_venv, shell=True)
-        tar = TAR_CMD.format(project_name, virtualenv_parent, project_name)
+        version = gitversion.get_version()
+        tar = TAR_CMD.format(project_name, version, 
+                virtualenv_parent, project_name)
         subprocess.check_call(tar, shell=True)
         context = zmq.Context()
         sock = context.socket(zmq.PUSH)
         sock.connect(socket_addr)
-        sock.send(project_name)
+        sock.send_json(dict(project=project_name, version=version))
 
     finally:
         shutil.rmtree(base_dir)
