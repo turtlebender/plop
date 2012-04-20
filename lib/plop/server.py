08ec44c607e24f4a6db98d37caeae1358586343a
Tom Howe <turtlebender@gmail.com>
2012-04-20 09:41:16 -0500

    Moving code into method for server

--- lib/plop/server.py
+++ lib/plop/server.py
@@ -1,15 +1,25 @@
 import zmq
-import time
 import os
 import shutil
 
-context = zmq.Context()
-sock = context.socket(zmq.PULL)
-sock.bind("ipc:///git/tmp/build_complete")
-os.chown('/git/tmp/build_complete', -1, 1001)
-os.chmod('/git/tmp/build_complete', 0660)
-      
-message = sock.recv()
-while message:
-    shutil.copyfile('/git/tmp/{0}.tar.gz'.format(message), os.path.expanduser('~/{0}.tar.gz'.format(message)))
-    message = sock.recv()
+CONTEXT = zmq.Context()
+
+def serve():
+    """
+    Start listening for build updates and execute the appropriate
+    handlers.
+    """
+    sock = CONTEXT.socket(zmq.PULL)
+    sock.bind("ipc:///git/tmp/build_complete")
+    os.chown('/git/tmp/build_complete', -1, 1001)
+    os.chmod('/git/tmp/build_complete', 0660)
+    message = sock.recv_json()
+    while message:
+        template = '/git/tmp/{0}-{1}.tar.gz'
+        source_file = template.format(message['project'], message['version'])
+        shutil.copyfile(source_file,
+                os.path.expanduser('~/{0}.tar.gz'.format(message)))
+        message = sock.recv()
+
+if __name__ == '__main__':
+    serve()
